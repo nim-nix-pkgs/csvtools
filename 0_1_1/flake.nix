eb5aceed37d67d0d066cb21ef6894a671942d691
hugosenari <hugosenari@gmail.com>
2022-03-22 00:27:05 -0300

    chore: re-index ../nimpkgs/c/csvtools/0_1_1

--- 0_1_1/flake.nix
+++ 0_1_1/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Manage CSV files'';
+  description = ''Manage CSV files in Nim'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,7 +9,7 @@
   
   inputs.src-csvtools-0_1_1.flake = false;
   inputs.src-csvtools-0_1_1.owner = "andreaferretti";
-  inputs.src-csvtools-0_1_1.ref   = "refs/tags/0.1.1";
+  inputs.src-csvtools-0_1_1.ref   = "0_1_1";
   inputs.src-csvtools-0_1_1.repo  = "csvtools";
   inputs.src-csvtools-0_1_1.type  = "github";
   
